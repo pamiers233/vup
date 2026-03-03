8a5a48eb986da83dcb7881c37d00196508cdb777
pamiers233 <xdu808875@gmail.com>
2026-03-04 00:08:47 +0800

    Update vup.js

--- js/vup.js
+++ js/vup.js
@@ -2,13 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // 【档案数据库】以后要加人，直接按格式复制一行即可
     const vupData = [
         {
-            name: "君士坦丁",
+            name: "君士坦丁-欧乌米Oumi",
             avatar: "../img/js.jpg", // 建议放张头像在 assets 文件夹
-            tags: ["语言学", "音位学", "历史比较法"],
-            desc: "专注于自然语言的深层结构分析，致力于在虚拟空间重建巴别塔。其研究涵盖乔姆斯基生成语法及上古汉语声韵构拟。",
+            tags: ["心理学", "神学", "历史"],
+            desc: "心理学学生，研究者，爱好者，大众心理学研究者",
             links: [
-                { label: "Bilibili 主页", url: "https://space.bilibili.com/..." },
-                { label: "录播组", url: "#" }
+                { label: "Bilibili 主页", url: "https://space.bilibili.com/579861015?spm_id_from=333.337.0.0" },
             ]
         },
         {
@@ -48,4 +47,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     renderVUPs();
+
 });
